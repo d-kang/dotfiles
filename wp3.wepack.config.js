3b474335198b3be5ffc55b427dc339a534d0b8de
d-kang <david.kang714@gmail.com>
2018-02-27 21:30:29 -0800

    Add eslint and babelrc files

---
+++ wp3.wepack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+const {
+  URL
+} = require('./config');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css', '.scss'],
+  },
+  module: {
+    rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3001,
+    overlay: true,
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        URL
+      },
+    }),
+  ],
+};
